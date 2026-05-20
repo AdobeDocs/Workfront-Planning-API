0acfccbc4e99ec0dc0f08f71991178261aca472b
armenavagumyan <armen.avagumyan@gmail.com>
2026-05-20 20:30:42 +0000

    Deploying to gh-pages from @ AdobeDocs/Workfront-Planning-API@efc4b3572a6f5426cf21672350235ab630714085 🚀

---
+++ component---src-pages-api-v-2-md-8246950f5406932bd97d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdev_site_documentation_template=self.webpackChunkdev_site_documentation_template||[]).push([[491],{4946:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return m},default:function(){return p}});var o=e(58168),u=e(98587),r=(e(88763),e(15680)),a=e(83407),c=["components"],m={},s={_frontmatter:m},i=a.A;function p(t){var n=t.components,e=(0,u.A)(t,c);return(0,r.mdx)(i,(0,o.A)({},s,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-api-v-2-md-8246950f5406932bd97d.js.map
